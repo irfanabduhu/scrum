26adaebf35a56fb713edaf786c4ca8a1da1b0e43
Irfanul Hoque <irhq13@gmail.com>
2022-04-05 12:46:24 +0600

    Changed database to postgres

--- config/database.js
+++ config/database.js
@@ -1,14 +1,8 @@
 const Sequelize = require("sequelize");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "config.env" });
-const {DBNAME, DBADMIN, DBPASS, DBHOST} = process.env;
 
 // Create a connection pool
-const db = new Sequelize("scrum", "postgres", "westcity", {
+const db = new Sequelize('scrum', 'postgres', 'westcity', {
   dialect: 'postgres',
-  // host: DBHOST,
-  // port: 3306,
 });
 
 module.exports = db;
